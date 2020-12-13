8d053871f36bc41f498b00dd766bbef733e7de25
Brandon Henry <bhenry@campaignmonitor.com>
2020-12-13 18:41:35 -0500

    solved

--- 13/code2.clj
+++ 13/code2.clj
@@ -1,23 +1,42 @@
 (ns thirteen)
 
 (def input "7,13,x,x,59,x,31,19")
-(def input "17,x,13,19")
-(def input "67,7,59,61")
-(def input "67,x,7,59,61")
-(def input "67,7,x,59,61")
-(def input "1789,37,47,1889")
+; (def input "17,x,13,19")
+; (def input "67,7,59,61")
+; (def input "67,x,7,59,61")
+; (def input "67,7,x,59,61")
+; (def input "1789,37,47,1889")
 
-(def input
- "13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23")
-
-(defn bint [n]
-(println "n=" n)
-  (bigint (Integer. n)))
+; (def input
+;  "13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23")
 
 (defn abs [i]
   (if (> 0 i)
     (* -1 i)
     i))
+
+(defn gcd "(gcd a b) returns the greatest common divisor of a and b" [a b]
+  (if (or (not (integer? a)) (not (integer? b)))
+    (throw (IllegalArgumentException. "gcd requires two integers"))
+    (loop [a (abs a) b (abs b)]
+      (if (zero? b) a,
+	  (recur b (mod a b))))))
+
+(defn lcm*
+  "(lcm a b) returns the least common multiple of a and b"
+  [a b]
+  (when (or (not (integer? a)) (not (integer? b)))
+    (throw (IllegalArgumentException. "lcm requires two integers")))
+  (cond (zero? a) 0
+        (zero? b) 0
+        :else (abs (* b (quot a (gcd a b))))))
+
+(defn lcm [xs]
+  (reduce lcm* (first xs) (rest xs)))
+
+(defn bint [n]
+(println "n=" n)
+  (bigint (Integer. n)))
 
 (defn extended-gcd
   "The extended Euclidean algorithm
@@ -75,10 +94,11 @@
       (if (meets? ts requirements)
         ts
         (if (meets? ts target-reqs)
-          (let [new-incr (apply * (map (fn [[a b]] (- b a)) target-reqs))]
+          (let [new-incr (lcm (map second target-reqs))]
             (recur (+ ts new-incr) new-incr (inc found)))
           (recur (+ ts incr) incr found))))))
 
+(print found-ts)
 (def p (apply * (map second requirements)))
 (loop [start found-ts]
   (if (> 0 (- start p))
