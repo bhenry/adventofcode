636e50eb24f03cf25c17a3420e27804191f665d0
Brandon Henry <bhenry@campaignmonitor.com>
2020-12-13 02:08:17 -0500

    try with lcd

--- 13/code2.clj
+++ 13/code2.clj
@@ -1,12 +1,8 @@
 (ns thirteen)
 
-(def ts 939)
 (def input "7,13,x,x,59,x,31,19")
 
-(def input "67,7,59,61")
-
-; (def ts 1008713)
- (def input
+(def input
  "13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23")
 
 (def buses (map #(Integer. %) (filter #(not (= "x" %)) (clojure.string/split input #","))))
@@ -17,10 +13,9 @@
 (def found (atom nil))
 
 (defn meets? [ts [i b]]
-  (when (= 0 (mod ts 10000))
-    (= 0 (mod (+ ts i) (Integer. b)))))
+  (= 0 (mod (+ ts i) (Integer. b))))
 
-(doseq [ts (range 100000000000000 1000000000000000000)]
+(doseq [ts (range 700404415482811 1000000000000000000000000 (Integer. (second (first requirements))))]
   (when (every? (partial meets? ts) requirements)
     (reset! found ts)
     (println ts)
