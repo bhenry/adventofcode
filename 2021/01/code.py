37dba3b6c41373169bd9e6d4c0d2e2c07421ede8
Brandon Henry <bhenry@campaignmonitor.com>
2022-12-02 10:21:35 -0500

    Day3 setup (#3)
    * day3
    
    * remove cruft
    
    * cleanup template
    
    * verify process from get-go
    
    * mv

--- 2021/01/code.py
+++ 2021/01/code.py
@@ -2,23 +2,39 @@ import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
 
-sample_input = """
+sample_input = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263
 """
 
-sample_answer1 = None
+sample_answer1 = 7
 sample_answer2 = None
 
 def process(input):
-    lines = [i.strip() for i in input.splitlines()]
-    return lines
-
-print(process(sample_input))
+    input = [int(i.strip()) for i in input.splitlines() if i.strip()]
+    return input
 
 def p1(input):
     data = process(input)
+    incs = 0
+    last = None
+    print(data)
+    for d in data:
+        if last and d > last:
+            incs += 1
+        last = d
+    return incs
 
 def p2(input):
     data = process(input)
+    pass
 
 if sample_answer1:
     print("sample test", p1(sample_input) == sample_answer1)
--- 2022/03/code.py
+++ 2022/03/code.py
@@ -1,25 +1,22 @@
 import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
-print(input)
-input_lines = input.splitlines()
 
 sample_input = """
-""".splitlines()
+"""
 
 sample_answer1 = None
 sample_answer2 = None
 
 def process(input):
-    pass
+    lines = [i.strip() for i in input.splitlines()]
+    return lines
 
 def p1(input):
     data = process(input)
-    pass
 
 def p2(input):
     data = process(input)
-    pass
 
 if sample_answer1:
     print("sample test", p1(sample_input) == sample_answer1)
