e5ab3933a3e5b88b8f08df134c1f2ca248b9350f
bhenry <bhenry@campaignmonitor.com>
2022-12-03 23:49:40 -0500

    midle of old day3

--- 2021/03/code.py
+++ 2021/03/code.py
@@ -17,7 +17,7 @@ sample_input = """00100
 """
 
 sample_answer1 = 198
-sample_answer2 = None
+sample_answer2 = 230
 
 def process(input):
     return [i.strip() for i in input.splitlines()]
@@ -25,8 +25,7 @@ def process(input):
 def binary_string_to_decimal(binary_string):
     return int(binary_string, 2)
 
-def p1(input):
-    data = process(input)
+def ge(data):
     first = data.pop()
     g = []
     for i in first:
@@ -37,14 +36,28 @@ def p1(input):
             g[i] += 1 if c == "1" else -1
     gamma = [1 if i > 0 else 0 for i in g]
     epsilon = [1 if i < 0 else 0 for i in g]
-    return binary_string_to_decimal("".join(str(i) for i in gamma)) * binary_string_to_decimal("".join(str(i) for i in epsilon))
+    return (gamma, epsilon)
+
+def p1(input):
+    data = process(input)
+    gamma, epsilon = ge(data)
+    gamma = binary_string_to_decimal("".join(str(i) for i in gamma))
+    epsilon = binary_string_to_decimal("".join(str(i) for i in epsilon))
+    return gamma * epsilon
+
+def getnext(data, z):
+    gamma, epsilon = ge(data)
+    g = [i for i in data if i[:z] == gamma[:z]]
+    e = [i for i in data if i[:z] == epsilon[:z]]
+    return (g, e)
 
 def p2(input):
     data = process(input)
+    return
 
 if sample_answer1:
     print("sample test", p1(sample_input) == sample_answer1)
     print("Problem1", p1(input))
 if sample_answer2:
     print("sample test2", p2(sample_input) == sample_answer2)
-    print("Problem2", p2(input))
+    # print("Problem2", p2(input))
