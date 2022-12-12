f6406d53d8f8ad3ed955038733f15fbce397cadd
bhenry <bhenry@campaignmonitor.com>
2022-12-12 03:32:23 -0500

    p1

--- 2022/12/code.py
+++ 2022/12/code.py
@@ -1,31 +1,77 @@
 import os
+from collections import defaultdict
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
 
-sample_input = """
+sample_input = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
 """
 
-sample_answer1 = None
+sample_answer1 = 31
 sample_answer2 = None
 
-def process(input):
-    return [i.strip() for i in input.splitlines()]
+def neighbors(loc):
+    return [(loc[0]+dx, loc[1]+dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
 
-# print(process(sample_input))
+def process(input):
+    grid = defaultdict(str)
+    for i, r in enumerate(input.splitlines()):
+        for c in range(len(r)):
+            grid[(i,c)] = r[c]
+            if r[c] == 'E':
+                dest = (i,c)
+            if r[c] == 'S':
+                start = (i,c)
+    return grid, dest, start
+
+def step(grid, loc, start):
+    new_points = set()
+    for point in neighbors(loc):
+        curheight = ord(grid[loc])
+        curheight = ord('z') if grid[loc] == 'E' else curheight
+        curheight = ord('a') if grid[loc] == 'S' else curheight
+        neighborheight = ord(grid[point]) if grid[point] else -100
+        neighborheight = ord('z') if grid[point] == 'E' else neighborheight
+        neighborheight = ord('a') if grid[point] == 'S' else neighborheight
+        if curheight - neighborheight in [1, 0] or curheight < neighborheight:
+            if point == start:
+                return set([point])
+            new_points.add(point)
+    return new_points # the options we can go to in this step
 
 def p1(input):
-    data = process(input)
-    return data
+    grid, dest, start = process(input)
+    rounds = 1
+    visited = set([dest])
+    new_options = step(grid, dest, start) - visited
+    visited.update(new_options)
+    for round in range(10000):
+        rounds += 1
+        temp_options = set()
+        for option in new_options:
+            temp_options.update(step(grid, option, start))
+        new_options = temp_options - visited
+        if start in new_options:
+            return rounds
+        visited.update(new_options)
+
+        # print(new_options - visited)
+        # print(visited)
+
+
 
 def p2(input):
     data = process(input)
     return data
 
 if sample_answer1:
-    sample_result = p1(sample_input)
-    print("sample1 test", sample_result == sample_answer1)
-    print("sample1", p1(sample_input))
-    # print("Problem1", p1(input), "\n\n")
+    # sample_result = p1(sample_input)
+    # print("sample1 test", sample_result == sample_answer1)
+    # print("sample1", p1(sample_input))
+    print("Problem1", p1(input), "\n\n")
 if sample_answer2:
     sample_result = p2(sample_input)
     print("sample2 test", sample_result == sample_answer2)
