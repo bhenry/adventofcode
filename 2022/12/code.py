6356c704332c7ce8baa33591bb3f85f361e05125
bhenry <bhenry@campaignmonitor.com>
2022-12-12 03:46:53 -0500

    p2

--- 2022/12/code.py
+++ 2022/12/code.py
@@ -11,7 +11,7 @@ abdefghi
 """
 
 sample_answer1 = 31
-sample_answer2 = None
+sample_answer2 = 29
 
 def neighbors(loc):
     return [(loc[0]+dx, loc[1]+dy) for dx, dy in [[-1, 0], [1, 0], [0, -1], [0, 1]]]
@@ -27,7 +27,7 @@ def process(input):
                 start = (i,c)
     return grid, dest, start
 
-def step(grid, loc, start):
+def step(grid, loc, start=None):
     new_points = set()
     for point in neighbors(loc):
         curheight = ord(grid[loc])
@@ -37,8 +37,8 @@ def step(grid, loc, start):
         neighborheight = ord('z') if grid[point] == 'E' else neighborheight
         neighborheight = ord('a') if grid[point] == 'S' else neighborheight
         if curheight - neighborheight in [1, 0] or curheight < neighborheight:
-            if point == start:
-                return set([point])
+            if point == start or (not start and curheight == ord('a')):
+                return "DONE"
             new_points.add(point)
     return new_points # the options we can go to in this step
 
@@ -64,16 +64,34 @@ def p1(input):
 
 
 def p2(input):
-    data = process(input)
-    return data
+    grid, dest, _ = process(input)
+    rounds = 0
+    visited = set([dest])
+    new_options = step(grid, dest) - visited
+    visited.update(new_options)
+    for round in range(10000):
+        rounds += 1
+        temp_options = set()
+        for option in new_options:
+            next_step = step(grid, option)
+            if "DONE" == next_step:
+                return rounds
+            temp_options.update(next_step)
+        new_options = temp_options - visited
+        if "DONE" == new_options:
+            return rounds
+        visited.update(new_options)
 
-if sample_answer1:
+        # print(new_options - visited)
+        # print(visited)
+
+# if sample_answer1:
     # sample_result = p1(sample_input)
     # print("sample1 test", sample_result == sample_answer1)
     # print("sample1", p1(sample_input))
-    print("Problem1", p1(input), "\n\n")
+    # print("Problem1", p1(input), "\n\n")
 if sample_answer2:
     sample_result = p2(sample_input)
     print("sample2 test", sample_result == sample_answer2)
     print("sample2", p2(sample_input))
-    # print("Problem2", p2(input))
+    print("Problem2", p2(input))
