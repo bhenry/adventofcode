8ef22b09117d41399322bdf4379844b3ac37ab65
bhenry <bhenry@campaignmonitor.com>
2022-12-11 22:09:45 -0500

    p1done

--- 2021/07/code.py
+++ 2021/07/code.py
@@ -1,21 +1,26 @@
+from collections import Counter
 import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
 
-sample_input = """
-"""
+sample_input = """16,1,2,0,4,2,7,1,2,14"""
 
-sample_answer1 = None
+sample_answer1 = 37
 sample_answer2 = None
 
 def process(input):
-    return [i.strip() for i in input.splitlines()]
+    return [int(i) for i in input.strip().split(",")]
 
 # print(process(sample_input))
 
 def p1(input):
-    data = process(input)
-    return data
+    data = Counter(process(input))
+    options = [0] * (max(data.keys()) + 1)
+    for i in range(len(options)):
+        for pos, count in data.items():
+            fuel = abs(int(pos) - i)
+            options[i] += fuel * count
+    return min(options)
 
 def p2(input):
     data = process(input)
@@ -25,7 +30,7 @@ if sample_answer1:
     sample_result = p1(sample_input)
     print("sample1 test", sample_result == sample_answer1)
     print("sample1", p1(sample_input))
-    # print("Problem1", p1(input), "\n\n")
+    print("Problem1", p1(input), "\n\n")
 if sample_answer2:
     sample_result = p2(sample_input)
     print("sample2 test", sample_result == sample_answer2)
