68d554ca71729a72b7e602861b6d8f5c79a0149a
bhenry <bhenry@campaignmonitor.com>
2022-12-13 02:41:46 -0500

    p2done

--- 2022/13/code.py
+++ 2022/13/code.py
@@ -1,4 +1,5 @@
 from curses.ascii import isdigit
+import functools
 import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
@@ -29,53 +30,24 @@ sample_input = """[1,1,3,1,1]
 """
 
 sample_answer1 = 13
-sample_answer2 = None
+sample_answer2 = 140
 
 def process(input):
     return input.split("\n\n")
 
-# print(process(sample_input))
-
-# def compare(a, b):
-#     if isinstance(a, list) and isinstance(b, list):
-#         if not a and not b:
-#             return True
-#         if not a and b:
-#             return True
-#         if not b and a:
-#             return False
-#         if not isinstance(a[0], list) and not isinstance(b[0], list):
-#             if a[0] < b[0]:
-#                 return True
-#             if a[0] > b[0]:
-#                 return False
-#             if a[0] == b[0]:
-#                 return compare(a[1:], b[1:])
-#         if not isinstance(a[0], list) and isinstance(b[0], list):
-#             return compare([a[0]], b[0])
-#         if isinstance(a[0], list) and not isinstance(b[0], list):
-#             return compare(a[0], [b[0]])
-#         return compare(a[0], b[0])
-#     else:
-#         if not isinstance(a, list) and isinstance(b, list):
-#             return compare([a], b)
-#         if isinstance(a, list) and not isinstance(b, list):
-#             return compare(a, [b])
-#     return a < b
-
 def compare(a, b):
     if isinstance(a, list) and isinstance(b, list):
         if not a and not b:
-            return 1
-        if not a and b:
-            return 1
-        if not b and a:
             return -1
+        if not a and b:
+            return -1
+        if not b and a:
+            return 1
         if not isinstance(a[0], list) and not isinstance(b[0], list):
             if a[0] < b[0]:
-                return 1
-            if a[0] > b[0]:
                 return -1
+            if a[0] > b[0]:
+                return 1
             if a[0] == b[0]:
                 return compare(a[1:], b[1:])
         if not isinstance(a[0], list) and isinstance(b[0], list):
@@ -95,27 +67,58 @@ def compare(a, b):
         if a == b:
             return 0
 
+def compare(a,b):
+    if isinstance(a, int) and isinstance(b, int):
+        if a < b:
+            return -1
+        if a > b:
+            return 1
+        if a == b:
+            return 0
+    if isinstance(a, list) and isinstance(b, list):
+        for i in range(max(len(a), len(b))):
+            if i >= len(a):
+                return -1
+            if i >= len(b):
+                return 1
+            if c := compare(a[i], b[i]):
+                return c
+            i += 1
+        return 0
+    if not isinstance(a, list) and isinstance(b, list):
+        return compare([a], b)
+    if isinstance(a, list) and not isinstance(b, list):
+        return compare(a, [b])
+
 
 def p1(input):
     pairs = process(input)
     inorder = []
     for i, pair in enumerate(pairs):
         l,r = [eval(x) for x in pair.strip().split("\n")]
-        if compare(l,r) == 1:
+        if compare(l,r) == -1:
             inorder.append(i+1)
     return sum(inorder)
 
 def p2(input):
-    data = process(input)
-    return data
+    data = [eval(i) for i in input.split()]
+    data.append([[2]])
+    data.append([[6]])
+    ans = 1
+    for i, v in enumerate(sorted(data, key=functools.cmp_to_key(compare))):
+        if v == [[2]]:
+            ans *= i+1
+        if v == [[6]]:
+            ans *= i+1
+    return ans
 
 if sample_answer1:
-    # compare([[1],[2,3,4]], [[1],4])
-    # sample_result = p1(sample_input)
-    # print("sample1 test", sample_result == sample_answer1)
-    # print("sample1", sample_result)
-    # if sample_result == sample_answer1:
-    print("\nproblem1", p1(input), "\n\n")
+    sample_result = p1(sample_input)
+    print("sample1 test", sample_result == sample_answer1)
+    print("sample1", sample_result)
+    if sample_result == sample_answer1:
+        print("\nproblem1", p1(input), "\n\n")
+
 if sample_answer2:
     sample_result = p2(sample_input)
     print("sample2 test", sample_result == sample_answer2)
