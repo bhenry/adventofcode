d16bda1a5a43176b5a08fa889d06fb6ce1ffb981
bhenry <bhenry@campaignmonitor.com>
2022-12-14 02:41:47 -0500

    p1

--- 2022/14/code.py
+++ 2022/14/code.py
@@ -2,10 +2,11 @@ import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
 
-sample_input = """
+sample_input = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
 """
 
-sample_answer1 = None
+sample_answer1 = 24
 sample_answer2 = None
 
 def process(input):
@@ -13,9 +14,50 @@ def process(input):
 
 # print(process(sample_input))
 
+def fallzones(loc):
+    return [(loc[0]+dx, loc[1]+dy) for dx, dy in [[0, 1], [-1, 1], [1, 1]]]
+
+def step(g):
+    sand = g['sandloc']
+    for loc in fallzones(sand):
+        if g.get(loc) not in ['#', 'o']:
+            g['sandloc'] = loc
+            if loc[1] > g['deepest']:
+                return
+            # drop falling sand
+            step(g)
+            return
+    g[g['sandloc']] = 'o'
+    g['sandcount'] += 1
+    return
+
 def p1(input):
+    g = {'sandcount': 0, 'deepest': 0}
     data = process(input)
-    return data
+    for line in data:
+        segs = []
+        for seg in [seg.strip() for seg in line.split("->")]:
+            segs.append([int(i) for i in seg.split(",")])
+        for i in range(len(segs)-1):
+            seg = segs[i]
+            next_seg = segs[i+1]
+            if seg[0] == next_seg[0]:
+                for y in range(min(seg[1], next_seg[1]), max(seg[1], next_seg[1])+1):
+                    g[(seg[0], y)] = '#'
+                    g['deepest'] = max(g['deepest'], y)
+            else:
+                for x in range(min(seg[0], next_seg[0]), max(seg[0], next_seg[0])+1):
+                    g[(x, seg[1])] = '#'
+                    g['deepest'] = max(g['deepest'], seg[1])
+    sandstart = (500, 0)
+    for _ in range(1000):
+        g['sandloc'] = sandstart
+        # drop new sand
+        step(g)
+
+    print(g['sandcount'])
+
+    return g['sandcount']
 
 def p2(input):
     data = process(input)
