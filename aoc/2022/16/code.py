438d27f9b0823e882ddde7b1ca065bf2f146c21a
bhenry <bhenry@campaignmonitor.com>
2022-12-16 20:53:16 -0500

    closest so far

--- aoc/2022/16/code.py
+++ aoc/2022/16/code.py
@@ -16,53 +16,106 @@ Valve JJ has flow rate=21; tunnel leads to valve II
 
 sample_answer1 = 1651
 sample_answer2 = None
-
 class Valve():
     def __init__(self, line):
         self.name = line.split(" ")[1]
         self.flow_rate = int(line.split("=")[1].split(";")[0])
         self.tunnels = [t.strip(',') for t in line.split(' ')[line.split(' ').index('to') + 2:]]
-        self.open = False
 
-    def open(self):
-        self.open = True
+"""
+minute 0
+AA
 
-    def __repr__(self):
-        return f"Valve {self.name} rate={self.flow_rate}; tunnels {', '.join(self.tunnels)}\n"
+minute 1
+AAAA (open AA)
+AAII (move to II)
+AADD (move to DD)
+AABB (move to BB)
 
-    def moves(self, m, tally, opens):
-        if self.name not in opens:
-            ms = [(self.name, m, tally + self.flow_rate*(m-1), opens + [self.name])]
-        else:
-            ms = [] # don't waste time if already open
-        for t in self.tunnels:
-            ms.append((t, 0, tally, opens))
-        return ms
+minute 2
+AAAADD (move to DD)
+AAAABB (move to BB)
+AAAAII (move to II)
+AAIIJJ (move to JJ)
+--AAIIAA (move to AA)-- # only backtrack if there's nowhere new to go
 
+minute 3 explodes to ~100 paths
+"""
+STORE = dict() # (pos, open_valves, time_remaining): flowscore
+VALVES = {}
+def calc(opens, t):
+    flow = 0
+    for v in opens:
+        flow += VALVES[v].flow_rate*t
+    return flow
+
+
+def recur(v, t, opens):
+    if (v, t, tuple(opens)) in STORE:
+        return STORE[(v, t, tuple(opens))]
+    # given a valve and time remaining, calculate all the moves left
+    valve = VALVES[v]
+    if t <= 1: # no moves left, but one more minute of flow
+        ans = calc(opens, t)
+        STORE[(v, t, tuple(opens))] = ans
+        return ans
+    moves = [(tunnel, t-1, opens) for tunnel in valve.tunnels]
+    if v not in opens and valve.flow_rate > 0:
+        # we can open this valve
+        # total_flow_on_path += valve.flow_rate*(t-1)
+        moves.append((v, t-1, opens | {v}))
+    ans = max([calc(os, tr) for _,tr,os in moves])
+    STORE[(v, t, tuple(opens))] = ans
+    return ans
+
+def key(v, t, opens):
+    return (v, t, "".join(sorted(opens)))
+
+# def compute(v, t, opens):
+#     # given a valve and time remaining, calculate all the moves left
+#     if key(v, t, opens) in STORE:
+#         return STORE[key(v, t, opens)]
+#     maximum = 0
+#     valve = VALVES[v]
+#     if t <= 1: # no moves left, but one more minute of flow
+#         ans = calc(opens, t)
+#         STORE[key(v, t, opens)] = ans
+#         return ans
+#     moves = [(tunnel, t-1, opens) for tunnel in valve.tunnels]
+#     if v not in opens and valve.flow_rate > 0:
+#         # we can open this valve
+#         # total_flow_on_path += valve.flow_rate*(t-1)
+#         moves.append((v, t-1, opens & {v}))
+#     ans = max([calc(os, tr) for _,tr,os in moves])
+#     STORE[key(v, t, opens)] = ans
+#     return ans
+
+def compute(v, time, opened):
+    if key(v, time, opened) in STORE:
+        return STORE[key(v, time, opened)]
+    if time <= 0:
+        return 0
+    highest = 0
+    valve = VALVES[v]
+    if v not in opened:
+        flow = valve.flow_rate * (time - 1)
+        for tunnel in valve.tunnels:
+            if flow:
+                highest = max(highest, flow + compute(tunnel, time - 2, opened | set([v])))
+            highest = max(highest, compute(tunnel, time-1, opened))
+    STORE[key(v, time, opened)] = highest
+    return highest
 
 def process(input):
-    return [Valve(i.strip()) for i in input.strip().splitlines()]
+    for i in input.strip().splitlines():
+        VALVES[i.split(" ")[1]] = Valve(i)
+    return compute('AA', 30, set())
+
+# print(process(sample_input))
 
 def p1(input):
     data = process(input)
-    valves = {v.name: v for v in data}
-    highest = 0
-    cur = 'AA'
-    v = valves[cur]
-    initial_moves = v.moves(30, 0, [])
-    possible_moves_at_minute = {0: [initial_moves]} # first possible moves minute1
-    for m in range(30):
-        remainingm = 29 - m
-        possible_moves_at_minute[m+1] = []
-        for set_of_moves in possible_moves_at_minute[m]:
-            for cur,_,tally,opens in set_of_moves:
-                # print(m, len(set_of_moves), cur, tally, opens)
-                v = valves[cur]
-                possible_moves_at_minute[m+1].append((v.moves(remainingm, tally, opens)))
-                if tally > highest:
-                    highest = tally
-
-    return highest
+    return data
 
 def p2(input):
     data = process(input)
