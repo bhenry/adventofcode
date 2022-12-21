c37023837354368ee41f79fbb3afd93ca0e1d509
bhenry <bhenry@campaignmonitor.com>
2022-12-21 01:30:07 -0500

    p1

--- aoc/2022/21/code.py
+++ aoc/2022/21/code.py
@@ -2,24 +2,67 @@ import os
 path_to_day = os.path.dirname(__file__)
 with open(f'{path_to_day}/input.txt') as f: input = f.read()
 
-sample_input = """
+sample_input = """root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
 """
 
-sample_answer1 = None
-sample_answer2 = None
+sample_answer1 = 152
+sample_answer2 = 301
 
 def process(input):
     return [i.strip() for i in input.splitlines()]
 
 # print(process(sample_input))
+MONKEYS = {}
+class monkey():
+    def __init__(self, inputt):
+        self.id = inputt.split(":")[0]
+        v = inputt.split(":")[1].strip()
+        self.value = v
+
+
+    def do(self):
+        parts = self.value.split(" ")
+        if len(parts) == 1:
+            value = int(self.value)
+        else:
+            p1 = MONKEYS[parts[0]].do()
+            p2 = MONKEYS[parts[2]].do()
+            if parts[1] == "+":
+                value = p1[0] + p2[0]
+            elif parts[1] == "-":
+                value = p1[0] - p2[0]
+            elif parts[1] == "*":
+                value = p1[0] * p2[0]
+            elif parts[1] == "/":
+                value = p1[0] / p2[0]
+        return value, parts
+
 
 def p1(input):
     data = process(input)
-    return data
+    for i in data:
+        MONKEYS[i.split(":")[0]] = monkey(i)
+    return MONKEYS['root'].do()[0]
 
 def p2(input):
     data = process(input)
-    return data
+    for i in data:
+        MONKEYS[i.split(":")[0]] = monkey(i)
+    return MONKEYS['root'].do()[0]
 
 if sample_answer1:
     sample_result = p1(sample_input)
