ae15a1bdc9ee46ffb98be33454bae72f50ab1ba0
bhenry <bhenry@campaignmonitor.com>
2023-12-01 17:15:38 -0500

    cleanup

--- aoc/2015/07/code.py
+++ aoc/2015/07/code.py
@@ -6,34 +6,13 @@ from lib.util import Input
 path_to_day = os.path.dirname(__file__)
 puzzleinput = Input(f'{path_to_day}/input.txt')
 
-samples = {
-"""123 -> x
-456 -> y
-x AND y -> d
-x OR y -> e
-x LSHIFT 2 -> f
-y RSHIFT 2 -> g
-NOT x -> h
-NOT y -> i""": 65412,
-}
-sample2s = {
-
-}
-
 def process(pz):
     return pz.lines()
 
-RECURSION = {"loops": 0}
 CACHE = {}
 def parse(vals, data):
     if data in CACHE:
         return CACHE[data]
-    # if RECURSION["loops"] > 1000000:
-    #     print("TOO MUCH RECURSION", data)
-    #     return 0
-    RECURSION["loops"] += 1
-    RECURSION[data] = RECURSION.get(data, 0) + 1
-    RECURSION["parser"] = RECURSION.get("parser", "") + data + ", "
     if data.isnumeric():
         return int(data)
     elif v := vals.get(data.strip()):
@@ -71,46 +50,13 @@ def p1(pz):
     for k in vals:
         print(k, parse(vals, vals[k]))
 
-
-    return parse(vals, "a")
-
-def p2(pz):
-    vals = {}
-    for line in pz:
-        data, target = line.split(" -> ")
-        vals[target.strip()] = data.strip()
-
-    for k in vals:
-        print(k, parse(vals, vals[k]))
-
-
-    newB = parse(vals, "a")
     return parse(vals, "a")
 
 pzz = process(puzzleinput)
 
-if answer2 := p2(pzz):
-    print("\nproblem2", answer2, "\n\n")
+answer1 = p1(pzz)
+print("\nproblem1", answer1, "\n\n")
 
-    # debug
-    if sample2s:
-        for sample in sample2s:
-            sample_result = p2(process(Input(sample)))
-            print("sample2", sample_result)
-            if sample_result == sample2s[sample]:
-                print("sample2 test pass")
-
-if answer1 := p1(pzz):
-    print("\nproblem1", answer1, "\n\n")
-
-# debug
-if samples:
-    for sample in samples:
-        sample_result = p1(process(Input(sample)))
-        print("sample1", sample_result)
-        if sample_result == samples[sample]:
-            print("sample1 test pass")
-
-
-
-print("\ndone")
+CACHE = {"b": answer1}
+answer2 = p1(pzz)
+print("\nproblem2", answer2, "\n\n")
