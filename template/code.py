44603a0468375e98a12ba744afdc1e740d538a16
bhenry <bhenry@campaignmonitor.com>
2023-12-01 10:21:16 -0500

    fix template

--- template/code.py
+++ template/code.py
@@ -1,6 +1,6 @@
 import os
 import sys
-APP_DIR = os.path.dirname(os.path.abspath(__file__).split("aoc")[0])
+APP_DIR = os.path.abspath(__file__).split("adventofcode")[0]+"adventofcode"
 sys.path.append(APP_DIR)
 from lib.util import Input
 path_to_day = os.path.dirname(__file__)
@@ -15,39 +15,37 @@ sample2s = {
 
 def process(pz):
     return pz.lines()
-
-def p1(pz):
-
-    return None
-
-def p2(pz):
-
-    return None
-
 pzz = process(puzzleinput)
 
-if answer2 := p2(pzz):
+def problem1(pz):
+
+    return None
+
+def problem2(pz):
+
+    return None
+
+# debug
+if sample2s:
+    for sample in sample2s:
+        sample_result = problem2(process(Input(sample)))
+        print("sample2", sample_result)
+        if sample_result == sample2s[sample]:
+            print("sample2 test pass")
+
+if answer2 := problem2(pzz):
     print("\nproblem2", answer2, "\n\n")
 
-    # debug
-    if sample2s:
-        for sample in sample2s:
-            sample_result = p2(sample)
-            print("sample2", sample_result)
-            if sample_result == sample2s[sample]:
-                print("sample2 test pass")
+# debug
+if samples:
+    for sample in samples:
+        sample_result = problem1(process(Input(sample)))
+        print("sample1", sample_result)
+        if sample_result == samples[sample]:
+            print("sample1 test pass")
 
-if answer1 := p1(pzz):
+if answer1 := problem1(pzz):
     print("\nproblem1", answer1, "\n\n")
-
-    # debug
-    if samples:
-        for sample in samples:
-            sample_result = p1(sample)
-            print("sample1", sample_result)
-            if sample_result == samples[sample]:
-                print("sample1 test pass")
-
 
 
 print("\ndone")
