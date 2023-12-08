8237495a50c2603c0ff020e90bc9627314397e1f
bhenry <bhenry@campaignmonitor.com>
2023-12-07 20:12:46 -0500

    cleanup

--- aoc/2015/10/code.py
+++ aoc/2015/10/code.py
@@ -15,33 +15,34 @@ sample2s = {
 
 def process(pz):
     return pz.lines()
+pzz = process(puzzleinput)
 
 def problem1(pz):
-    lines = process(pz)
+    pass
 
 def problem2(pz):
-    lines = process(pz)
+    pass
 
 # debug
 if sample2s:
     for sample in sample2s:
-        sample_result = problem2(Input(sample))
+        sample_result = problem2(process(Input(sample)))
         print("sample2", sample_result)
         if sample_result == sample2s[sample]:
             print("sample2 test pass")
 
-if answer2 := problem2(puzzleinput):
+if answer2 := problem2(pzz):
     print("\nproblem2", answer2, "\n\n")
 
 # debug
 if samples:
     for sample in samples:
-        sample_result = problem1(Input(sample))
+        sample_result = problem1(process(Input(sample)))
         print("sample1", sample_result)
         if sample_result == samples[sample]:
             print("sample1 test pass")
 
-if answer1 := problem1(puzzleinput):
+if answer1 := problem1(pzz):
     print("\nproblem1", answer1, "\n\n")
 
 
