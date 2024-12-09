82abe45fd2dea880f83613b42e5c0a20bc7d40dd
bhenry <bhenry@campaignmonitor.com>
2024-12-09 03:06:47 -0500

    p1d9

--- aoc/2024/09/code.py
+++ aoc/2024/09/code.py
@@ -9,7 +9,7 @@ puzzleinput = Input(f'{path_to_day}/input.txt')
 line = puzzleinput.lines()[0]
 
 sample = "2333133121414131402"
-line = sample
+# line = sample
 
 part1 = 0
 
@@ -27,11 +27,23 @@ else:
 
 mapp = []
 for i, f in enumerate(list(fs)):
-    mapp.append(f'{i}' * f[0])
-    mapp.append('.' * f[1] if f[1] > 0 else '')
+    for j in range(f[0]):
+        mapp.append(i)
+    for j in range(f[1]):
+        mapp.append('.')
 
-print(mapp)
-print("".join(mapp))
+newmap = []
 
+while "." in mapp:
+    left = mapp.index('.')
+    right = len(mapp) - 1
+    mapp[left], mapp[right] = mapp[right], mapp[left]
+    while mapp[-1] == '.':
+        mapp.pop()
+
+for i, n in enumerate(mapp):
+    part1 += int(n) * (i)
+
+print(part1)
 
 part2 = 0
