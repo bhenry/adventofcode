0bb86e5d3ecb417a097fec77071a9aa611413a53
bhenry <bhenry@campaignmonitor.com>
2024-12-12 23:12:54 -0500

    wip. works on sample

--- aoc/2024/12/code.py
+++ aoc/2024/12/code.py
@@ -18,6 +18,12 @@ VVIIICJJEE
 MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE"""
+# sample="""AAAAAA
+# AAABBA
+# AAABBA
+# ABBAAA
+# ABBAAA
+# AAAAAA"""
 lines = sample.strip().split("\n")
 
 g = [list(x) for x in lines]
@@ -73,4 +79,59 @@ print(part1)
 part2 = 0
 
 for region in regions:
-    pass
+    x,y = next(iter(region))
+    value = g[y][x]
+    area = len(region)
+    outers = {}
+    inners = {}
+    for x,y in region:
+        for nx,ny in neighbors(x,y):
+            if (nx,ny) not in region:
+                if (x,y) in outers:
+                    outers[(x,y)].append((nx,ny))
+                else:
+                    outers[(x,y)] = [(nx,ny)]
+                if (nx,ny) in inners:
+                    inners[(nx,ny)].append((x,y))
+                else:
+                    inners[(nx,ny)] = [(x,y)]
+    print(region)
+    corners = set()
+    for outer in outers:
+        if len(outers[outer]) == 4:
+            cs = [(outer[0]+a, outer[1]+b) for a,b in [(0,0.5),(0.5,0),(-0.5,0),(0,-0.5)]]
+            corners.update(cs)
+            continue
+        if len(outers[outer]) == 3:
+            connecting_side = set(neighbors(*outer)) - set(outers[outer])
+            x1,y1 = next(iter(connecting_side))
+            x0,y0 = outer
+            dx,dy = x1-x0, y1-y0
+            corners.add((x0-dx/2, y0-dy/2))
+            corners.add((x0+dx/2, y0+dy/2))
+            continue
+        if len(outers[outer]) == 2:
+            x0,y0 = outers[outer][0]
+            x1,y1 = outers[outer][1]
+            if abs(x0-x1) == 1 and abs(y0-y1) == 1:
+                cs = ((x0+x1)/2, (y0+y1)/2)
+                if cs not in corners:
+                    corners.add(cs)
+                else:
+                    corners.remove(cs)
+    corners = len(corners)
+    for inner in inners:
+        if len(inners[inner]) == 4:
+            corners += 4
+            continue
+        if len(inners[inner]) == 3:
+            corners += 2
+            continue
+        if len(inners[inner]) == 2:
+            x0,y0 = inners[inner][0]
+            x1,y1 = inners[inner][1]
+            if abs(x0-x1) == 1 and abs(y0-y1) == 1:
+                corners += 1
+    print(region, "===\n\n\n", area, corners, area*corners)
+    part2 += area * corners
+print(part2)
