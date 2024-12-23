615d977b655b3b52044e487d392a1ae02eb89610
bhenry <bhenry@campaignmonitor.com>
2024-12-23 14:38:56 -0500

    d23 done

--- aoc/2024/22/code.py
+++ aoc/2024/22/code.py
@@ -8,12 +8,28 @@ raw = rawfile(f'{path_to_day}/input.txt')
 
 lines = raw.split("\n")
 
-sample = """
+sample = """1
+10
+100
+2024
 """
-# lines = sample.strip().split("\n")
+lines = sample.strip().split("\n")
 
 part1 = 0
 
+"""
+1: 8685429
+10: 4700978
+100: 15273692
+2024: 8667524
+Adding up the 2000th new secret number for each buyer produces 37327623.
+"""
+
+def mix(a, b):
+    return a ^ b
+
+def prune(x):
+    return x % 16777216
 
 
 part2 = 0
