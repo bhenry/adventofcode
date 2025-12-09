419c7b0f5cee42c64b2633c3848470ff38ba3750
bhenry <bhenry@campaignmonitor.com>
2025-12-09 16:54:45 -0500

    day 9 part 2 works on sample

--- aoc/2025/09/code.py
+++ aoc/2025/09/code.py
@@ -18,8 +18,8 @@ sample = """7,1
 2,3
 7,3
 """
-# lines = sample.strip().split("\n")
-starttime = datetime.now()
+# lines = sample.strip()  .split("\n")
+
 part1 = 0
 coords = [nums(line) for line in lines]
 areas = set()
@@ -30,68 +30,44 @@ for x,y in coords:
         areas.add((abs(x - a)+1) * (abs(y - b) +1))
 part1 = max(areas)
 print(part1)
-endtime = datetime.now()
-print("Time:", endtime - starttime)
+
 part2 = 0
+border = set()
+for i in range(len(coords)):
+    for j in range(i+1, len(coords)):
+        x1, y1 = coords[i]
+        x2, y2 = coords[j]
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2)+1):
+                border.add((x1, y))
+        if y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2)+1):
+                border.add((x, y1))
+border = list(border)
+greens = set()
+for i in range(len(border)):
+    for j in range(i+1, len(border)):
+        x1, y1 = border[i]
+        x2, y2 = border[j]
+        if x1 == x2:
+            for y in range(min(y1, y2), max(y1, y2)+1):
+                greens.add((x1, y))
+        if y1 == y2:
+            for x in range(min(x1, x2), max(x1, x2)+1):
+                greens.add((x, y1))
 
-max_x = max([x for x,y in coords])
-max_y = max([y for x,y in coords])
-min_x = min([x for x,y in coords])
-min_y = min([y for x,y in coords])
-
-for x in range(min_x, max_x + 1):
-    for y in range(min_y, max_y + 1):
-        # if x,y between greens or coords then add to greens
-        if (x,y) in coords:
-            # greens.add((x,y))
-            continue
-        if (x,y) in greens:
-            continue
-        for g in gs:
-            # if x,y is between any two greens in same row or column
-            if x == g[0]:
-                if (x, min(y, g[1]) + 1) in gs and (x, max(y, g[1]) - 1) in gs:
-                    greens.add((x,y))
-                    break
-            if y == g[1]:
-                if (min(x, g[0]) + 1, y) in gs and (max(x, g[0]) - 1, y) in gs:
-                    greens.add((x,y))
-                    break
-
-# for (x,y) in greens.copy():
-#     for (a,b) in greens.copy():
-#         if x == a:
-#             for i in range(1,abs(y - b)):
-#                 greens.add((x,y+i if y < b else y - i))
-#         if y == b:
-#             for i in range(1,abs(x - a)):
-#                 greens.add((x+i if x < a else x - i,y))
-areas = set()
-greens = list(greens)
-print("coords:", coords)
-print("greens:", greens)
-for x,y in coords:
-    for a,b in coords:
-        valid = True
-        if (x,y) == (a,b):
-            continue
-        for i in range(1,abs(y - b)):
-            if (x,y+i if y < b else y - i) not in greens + coords:
-                # if (x,y) == (9,5) and (a,b) == (2,3):
-                #     print("i",x,y,a,b,i)
-                valid = False
-                break
-        if not valid:
-            continue
-        for j in range(1,abs(x - a)):
-            if (x+j if x < a else x - j,y) not in greens + coords:
-                # if (x,y) == (9,5) and (a,b) == (2,3):
-                #     print("j",x,y,a,b,j)
-                valid = False
-                break
-        if valid:
-            # print((x,y), (a,b))
-            areas.add((abs(x - a)+1) * (abs(y - b)+1))
-
-part2 = max(areas)
+for i in range(len(coords)):
+    for j in range(i+1, len(coords)):
+        x1, y1 = coords[i]
+        x2, y2 = coords[j]
+        fail = False
+        for x in range(min(x1, x2), max(x1, x2)+1):
+            for y in range(min(y1, y2), max(y1, y2)+1):
+                if (x, y) not in greens:
+                    fail = True
+                    continue
+        if not fail:
+            area = (abs(x1 - x2)+1) * (abs(y1 - y2) +1)
+            if area > part2:
+                part2 = area
 print(part2)
